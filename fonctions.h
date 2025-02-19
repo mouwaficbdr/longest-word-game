445e3557dad3055c060e3af33b5c6f3c5782439b
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-02-19 02:12:49 +0100

    Ajout du fichier fonctions.h avec la déclaration des constantes, variables et fonctions pour le jeu

---
+++ fonctions.h
@@ -0,0 +1,20 @@
+#ifndef FONCTIONS_H
+#define FONCTIONS_H
+
+// Déclaration des constantes
+extern const int nbreJoueur;
+extern const int nbreTotalLettresGrille;
+extern const char voyelles[6];
+extern const char consonnes[20];
+
+// Déclaration des variables
+extern char nomJoueur1[10];
+extern char nomJoueur2[10];
+extern int numJoueurCommencerPartie;
+extern int nbreTours;
+extern char choixConsonneVoyelle;
+
+// Déclaration des fonctions
+
+
+#endif
