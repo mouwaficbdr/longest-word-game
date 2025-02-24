1fcb48443a1006fc83316fb1cfc3af3ede09446c
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-02-24 17:44:59 +0100

    Ajouter les prototypes des fonctions de sauvegarde et de chargement de partie

--- fonctions.h
+++ fonctions.h
@@ -15,6 +15,7 @@ extern int nbreTours;
 extern char choixConsonneVoyelle;
 
 // Déclaration des fonctions
-
+void sauvegarderPartie(const char *nomJoueur1, const char *nomJoueur2, int scoreJoueur1, int scoreJoueur2, int toursJoues, int toursTotaux, const char grille[9]);
+int chargerPartie(char *nomJoueur1, char *nomJoueur2, int *scoreJoueur1, int *scoreJoueur2, int *toursJoues, int *toursTotaux, char grille[9]);
 
 #endif
