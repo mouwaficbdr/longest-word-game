59e3cc543ab68ce4aeac90b45ff6c904e08cb4b1
Andy ATOHOUN <andymfrd02@gmail.com>
2025-02-24 18:57:07 +0100

    Mise en place de la fonction de validation des caractères du mot & validation du mot dans le dictionnaire

--- fonctions.c
+++ fonctions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "fonctions.h"
 
 // Définition des constantes
@@ -16,3 +17,82 @@ int nbreTours;
 char choixConsonneVoyelle;
 
 // Définition des fonctions
+
+//Fonction de vérification des caractères
+char * validationChar(char mot[], char grilleCaractere[]){
+    char validCarac[9];
+    int isequal, i;
+
+    //On parcourt le tableau contenant le mot de l'utilisateur
+    while(mot[i] != "\0"){
+
+        //On initialise à 0 pour dire que le caractère à l'indice i n'est égale à aucun parmi ceux de la grille
+        isequal = 0;
+
+        //On parcourt la grille de caractères
+        for(int j = 0; j < nbreTotalLettresGrille; j++){
+
+            //Si le caractère à l'indice i correspond à un caractère parmi ceux de la grille, isequal passe à 1 pour dire vrai
+            //on vide la case contenant le caractère dans la grille
+            //on passe au caractère suivant
+            if(mot[i] == grilleCaractere[j]){
+                isequal = 1;
+                grilleCaractere[j] = "\0";
+                break;
+            }
+        }
+
+        //Si la variable isequal est toujours à 0, alors le caractère dans le mot de l'utilisateur n'a pas de correspondant dans la grille
+        //On retourne la valeur 0 pour stopper la validation
+        //Le mot ne respecte pas les caractères de la grille 
+        if(isequal == 0){
+            return 0;
+        }
+        i++;
+    }
+
+    //si tout s'est bien passé, on renvoie le mot valide qui respecte les caractères de la grille
+    return mot;
+}
+
+
+void validationMots(char mot[]){
+    int *position, i = 0;
+    //Pointeur sur le fichier du dictionnaire
+    FILE * dico = NULL; 
+
+    //Ouverture du dictionnaire
+    dico = fopen("dico.txt", "r");
+    int notFound = 0;
+    char motDico[9] = "";
+    if(dico != NULL){
+        fseek(dico, 0, SEEK_END);
+        *position = ftell(dico);
+        fseek(dico, *position/2, SEEK_SET);
+        do{
+            fgets(motDico, 9, dico);
+            if(strcmp(mot, motDico) == 0){
+                notFound = 1;
+            }else{
+                if(strcmp(mot, motDico) < 0){
+                    *position = ftell(dico);
+                    fseek(dico, -(*position/2), SEEK_CUR);
+                }else{
+                    *position = ftell(dico);
+                    fseek(dico, (*position/2), SEEK_CUR);
+                }
+            }
+        }while(!notFound);
+
+        if(notFound){
+            while(mot[i] != "\0"){
+                i++;
+                return i;
+            }
+        }else{
+            return 0;
+        }
+
+        fclose(dico);
+    }
+}
