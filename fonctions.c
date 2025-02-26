79c12de162842c92c4a102d9a0534d327a04c9ce
Seathiel <ogoudedjimonde@gmail.com>
2025-02-26 21:55:28 +0100

    Prototypage et déclaration des fonctions nécessaires pour les anagrammes et l'arbre de possibilités

--- fonctions.c
+++ fonctions.c
@@ -16,3 +16,87 @@ int nbreTours;
 char choixConsonneVoyelle;
 
 // Définition des fonctions
+
+
+// Spécification: ici on cherche a supprimer une seule occurence d'une lettre, pas elles toutes
+char *removeChar(char *str, char c) {
+    int j=0;
+    int taille=strlen(str);
+
+//Dès qu'on rencontre la première occurence de la lettre en question on la remplace et on arrête le parcours de la chaine
+    for(int i=0;i<taille;i++){
+        if(str[i]==c ){
+          str[i]='\n';
+          j++;
+          break;
+        }
+    }
+
+//On supprime maintenant ce seul caractère remplacé
+    j=0;
+    char temp;
+    for(int i=0;i<taille;i++){
+        temp= str[i];
+     if(temp=='\n'){
+         continue;
+     }
+     str[j]=temp;
+     j++;
+    }
+    str[taille-1]='\0';
+    return str;
+    }
+
+
+
+
+    motPossible *Anagramme_(motPossible *Noeud, int taille, char tableau[]){
+      //Allouer l'espace necessaire pour un tableau de mot possible qu'on associera au champ suivant du noeud à considérer  
+      motPossible *tableau_mot=malloc(sizeof(motPossible)*(9-taille+1));
+
+      //tableau des lettres restantes après suppression des caractères du parent
+      char *tableauR = malloc(sizeof(char)*(strlen(tableau)-taille+2));
+
+      //tableau intermédaire pour ne pas affecter le tableau des lettres générées par l'utilisateur
+      char *tableau_inter=malloc(sizeof(char)*strlen(tableau));
+      strcpy(tableau_inter,tableau);
+      
+        for (int i=0; i<taille-1; i++){
+          tableau_inter=removeChar(tableau_inter, Noeud->mot[i]);
+        }
+          strcpy(tableauR,tableau_inter);
+          free(tableau_inter);
+  
+          //créer les mots
+      for(int i=0; i<(10-taille); i++){
+          tableau_mot[i].mot=malloc(sizeof(char)*(taille+1));
+          strcpy(tableau_mot[i].mot, Noeud->mot);
+              tableau_mot[i].mot[taille-1]=tableauR[i];
+              tableau_mot[i].mot[taille]='\0';
+              tableau_mot[i].suivant=NULL;
+      }
+  
+      //Allouer l'espace et lier au Noeud
+      Noeud->suivant=malloc(sizeof(motPossible)*(9-taille+1));
+      for (int i=0; i<(10-taille);i++){
+              Noeud->suivant[i]=malloc(sizeof(motPossible));
+              Noeud->suivant[i]->mot=malloc(sizeof(char)*(taille+1));
+              Noeud->suivant[i]=(tableau_mot+i);
+      }
+      return Noeud->suivant;
+    }
+
+
+  
+    void CreationArbre(motPossible *Noeud,int tailleGen, char* chaine){
+        motPossible *pointeur=malloc(sizeof(motPossible));
+        pointeur=Noeud;
+        pointeur->suivant=Anagramme_(pointeur,tailleGen,chaine);
+
+        //Pour chaque génération si le mot généré n'est pas de taille maximale (soit 9) on crée toute la génération suivante, l'itération ici permet de parcourir tous les noeuds d'une génération pour les créer et la récursivité passe d'une racine vers le fils jusqu'à la base
+            for(int i=0; i<10-tailleGen; i++){
+            if(strlen(pointeur->suivant[i]->mot)!=9){
+                CreationArbre(pointeur->suivant[i],tailleGen+1,chaine);
+               }
+            }
+    }
--- fonctions.h
+++ fonctions.h
@@ -14,7 +14,60 @@ extern int numJoueurCommencerPartie;
 extern int nbreTours;
 extern char choixConsonneVoyelle;
 
+//Declaration des structures
+
+/** 
+ * @brief
+ * motPossible
+ * @champ {char *} le mot généré par l'anagramme
+ * @champ {motPossible **} le pointeur vers le tabelau des fils du 
+ * généré a partir du mot précédent
+ */
+typedef struct motPossible {
+    char *mot;
+    struct motPossible **suivant;
+    } motPossible;
+  
+/**
+ * @brief 
+ * Possibilites 
+ * Liste chainée des mots possibles
+ * @champ {motPossible *} tableau e pointeur vers les premiers lettres soit les lettres générées
+ */
+    typedef struct Possibilites{
+     motPossible *premier[9];
+    }Possibilites;
+
+    
 // Déclaration des fonctions
 
 
+/**
+ * @brief 
+ * Supprimer un caractère d'une chaine 
+ * Pour supprimer les caractères déja utilisés par le noeud parent afin de produire un mot unique dans la génération suivante
+ * @param str la chaine 
+ * @param c le caractère a supprimer
+ * @return char* 
+ */
+char *removeChar(char *str, char c);
+
+/**
+ * @brief 
+ * Générer les anagrammes uniques possibles d'un noeud 
+ * @param Noeud Le noeud à considérer pour génerer ses fils 
+ * @param taille taille du tableau de lettres de la génération des noeuds à créer
+ * @param tableau tableau des lettres générées par les joueurs
+ * @return motPossible* le tableau des mots générés
+ */
+motPossible *Anagramme_(motPossible *Noeud, int taille, char tableau[]);
+
+/**
+ * @brief 
+ * Générer entièrement l'arbre en partant d'une racine jusqu'à sa base
+ * @param Noeud L'un des noeuds initial de l'arbre (de la 1ère génération)
+ * @param tailleGen taille du tableau de lettres de la génération à créer
+ * @param chaine le tableau des lettres générées par les joueurs
+ */
+void CreationArbre(motPossible *Noeud,int tailleGen, char* chaine);
 #endif
