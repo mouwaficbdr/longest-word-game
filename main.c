669a9d7cf74f8262ef03d9b5088ecd395deea816
Andy ATOHOUN <andymfrd02@gmail.com>
2025-03-03 15:34:38 +0100

    Commit des .json

--- main.c
+++ main.c
@@ -7,12 +7,5 @@
 
 int main()
 {
-    char mot[9] = "abaissa";
-    int result;
-    
-    // Affichage d'un message pour voir si le programme s'exécute
-    printf("Test de validation du mot '%s'...\n", mot);
-    
-    
-   return 0;
+    return 0;
 }
