edd39ee15c3df573fe71e2012f505f315f604424
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-03-03 01:13:41 +0100

    Amélioration du programme principal : ajout de messages de débogage et de pause utilisateur

--- main.c
+++ main.c
@@ -6,13 +6,23 @@
 
 int main()
 {
-    char mot[6] = "arbre";
-    int result;
-    result = validationMots(mot);
+   char mot[6] = "arbre";
+   int result;
+   
+   // Affichage d'un message pour voir si le programme s'exécute
+   printf("Test de validation du mot '%s'...\n", mot);
+   
+   result = validationMots(mot);
 
-    if(result == 0){
-        printf("Le mot n'xiste pas dans le dictionnaire");
-    }else{
-        printf("Votre score est de %d", result);
-    }
+   if(result == 0){
+      printf("Le mot n'existe pas dans le dictionnaire\n");
+   }else{
+      printf("Votre score est de %d\n", result);
+   }
+   
+   // Ajout d'une pause pour voir le résultat
+   printf("Appuyez sur Entrée pour continuer...");
+   getchar();
+   
+   return 0;
 }
