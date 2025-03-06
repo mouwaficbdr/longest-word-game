bc301e55d8d4043d227efc50a9aae0643499f840
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-03-06 23:53:18 +0100

    Nettoyage du code principal et simplification du point d'entrée
    - Suppression des commentaires de code obsolètes dans la fonction main()
    - Conservé uniquement l'appel à lancerJeu() comme point d'entrée principal
    - Amélioration de la lisibilité du fichier source

--- main.c
+++ main.c
@@ -6,11 +6,6 @@
 #include <string.h>
 
 int main(){
-  // initialiserVariables();
-  // afficherInterface();
-  // player1Score();printf("%d",scoreJoueur1);
-  // player2Score();printf("%d",scoreJoueur2);
-  // genererCaractereAleatoires();
   
   lancerJeu();  
   return 0;    
